4ad2a15435ddb9bf9e4b718a066ed13dfa9bb801
tenglely <1109927412@qq.com>
2019-10-15 17:34:22 +0800

    写了医务人员首页

--- src/main/java/com/blood/bloodservice/controller/PageJumpController.java
+++ src/main/java/com/blood/bloodservice/controller/PageJumpController.java
@@ -4,7 +4,6 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -24,11 +23,16 @@ public class PageJumpController {
         return "login";
     }
 
-    @GetMapping("/user" +
-            "/hello")
-    @ResponseBody
+    @ApiOperation(value = "医务人员登录页面")
+    @GetMapping("/doctorlogin")
+    public String doctorlogin(HttpServletResponse response) throws Exception{
+        return "/doctor/doctorlogin";
+    }
+
+
+    @GetMapping("/doctor/index")
     public String hello(){
-        return "hello";
+        return "/doctor/index";
     }
 
 }
